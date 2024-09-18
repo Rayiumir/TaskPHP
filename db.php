e593f9179b5a2326cdfb998c49ff56f0a5724d2d
Raymond Baghumian <swordart2050@gmail.com>
2024-09-18 03:48:41 +0330

    Fix Login

--- db.php
+++ db.php
@@ -10,6 +10,7 @@ try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
+    header("Location: ../index.php");
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
